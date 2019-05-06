540dcfef5e6e1b0f6698743ced8af7a480b7073d
leswing <lilleswing@gmail.com>
2019-05-06 18:56:25 -0400

    Use Web Worker

--- js/bfs.js
+++ js/bfs.js
@@ -9,6 +9,12 @@ var movies = {};
 
 var leftActor = "";
 var rightActor = "Kevin Bacon";
+var worker = new Worker('js/bfs_worker.js');
+
+worker.addEventListener('message', function(e) {
+  var path = e.data;
+  $("#myPath").html(e.data);
+}, false);
 
 $.ajax({
   url: "static/actors.json",
@@ -16,6 +22,9 @@ $.ajax({
   success: function(x) {
     actors = x;
     setRandomFromImage();
+    worker.postMessage({
+      "actors": actors
+    });
   }
 });
 
@@ -24,9 +33,11 @@ $.ajax({
   method: "GET",
   success: function(x) {
     movies = x;
+    worker.postMessage({
+      "movies": movies
+    });
   }
 });
-
 
 
 function logIt(x) {
@@ -56,62 +67,12 @@ function randomGif(s, imgId) {
 }
 
 
-function deepCopy(o) {
-   return JSON.parse(JSON.stringify(o));
-}
-
-function appendQueue(q, n, a, used) {
-  // q is the queu
-  // n is the current node
-  // a is the array to expand
-  // used: object holding keys in the queue
-  for (var i=0; i < a.length;  i++) {
-    var elem = a[i];
-    if (elem in used) {
-      continue
-    }
-    var copy = deepCopy(n);
-    copy['current'] = elem;
-    copy['path'].push(elem);
-    q.push(copy);
-    used[elem] = true;
-  }
-}
-
-function bfs_helper(q, dest) {
-  /*
-   * q is an array of \{"current": str, "path": array\}
-   * dest: string of goal actor
-   */
-  var used = {}
-  while (q.length > 0) {
-    var node = q.shift();
-    var current = node['current'];
-    if (current === dest) {
-      return node["path"];
-    }
-    if (node["path"].length > 8) {
-      return "No Path To " + dest + "!"
-    }
-    // Expand through movies
-    if (current in actors) {
-      var new_movies = actors[current];
-      appendQueue(q, node, new_movies, used);
-    } else if (current in movies) {
-      // Expand through actors
-      var new_actors = movies[current];
-      appendQueue(q, node, new_actors, used);
-    } else {
-      console.log(current + " not in actors or movies"); }
-  }
-}
-
-async function bfs(from_actor, to_actor) {
-  var node = {"current":from_actor, "path":[from_actor]};
-  var q = [node];
-  var path = bfs_helper(q, to_actor);
-  path = prettyList(path);
-  $("#myPath").html(path);
+function bfs_worker_call(from_actor, to_actor) {
+  var message = {
+    "from_actor": from_actor,
+    "to_actor": to_actor
+  };
+  worker.postMessage(message);
 }
 
 function prettyList(a) {
@@ -127,13 +88,11 @@ function prettyList(a) {
 function randomProperty(obj) {
     var keys = Object.keys(obj);
     var key_index = Math.floor(keys.length * Math.random());
-    console.log("Key Index:" + key_index);
     return keys[key_index]
 };
 
 function setRandomFromImage() {
   var myActor = randomProperty(actors);
-  console.log(myActor);
   $("#fromActor").attr("value", myActor);
   randomGif(myActor, "fromImage");
   leftActor = myActor;
@@ -151,7 +110,6 @@ $("#searchButton").click(function() {
     randomGif(toActor, "toImage");
   }
   if (!(fromActor in actors)) {
-    console.log("Missing");
     $("#myPath").html("We don't know " + fromActor);
     return
   }
@@ -161,11 +119,7 @@ $("#searchButton").click(function() {
   }
   var loadingHtml = "<img src='static/loading.gif' />"
   $("#myPath").html(loadingHtml);
-
-  setTimeout(function() {
-    console.log("timeout");
-    bfs(fromActor, toActor);
-  }, 1);
+  bfs_worker_call(fromActor, toActor);
 });
 
 randomGif("Kevin Bacon", "toImage");
